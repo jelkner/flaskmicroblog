436e20998e89ec7b74182dce238f9087aad9c704
Jeffrey Elkner <jeff@elkner.net>
2017-02-08 10:32:58 -0500

    Added fake posts to views.py and loop in index.html (and .gitignore)

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}
-    return render_template('index.html', title='Home', user=user) 
+    posts = [
+        {
+            'author': {'nickname': 'Amanuel'},
+            'body': "It's a beautiful day in the neighborhood!"
+        },
+        {
+            'author': {'nickname': 'Marisol'},
+            'body': "I hope we get a snow day before Winter ends!"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
