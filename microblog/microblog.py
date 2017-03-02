63a72f91b71bc60ca7c3a9ceb8a5298cf8a78272
Jeffrey Elkner <jeff@elkner.net>
2017-03-02 11:04:30 -0500

    Completed Step 5: Creating The Database

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -20,3 +20,32 @@ def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 from microblog import app
-app.run('0.0.0.0', debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
